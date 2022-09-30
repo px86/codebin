7679b239546701423cf8f20ce789cb5dd68f21f7
Pushkar Raj <px86@protonmail.com>
2022-09-30 21:51:46 +0530

    Move source files to `src/`

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const hljs = require("highlight.js");
+const path = require("path");
+
 const dbInit = require("./db");
 const { genUniqueId } = require("./utils");
 
@@ -11,8 +13,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (_req, res) => {
   res.render("index.ejs", { title: "Codebin", mode: "new" });
